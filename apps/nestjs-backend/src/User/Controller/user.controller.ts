459712d9b46f013c5340f5163c1fa13a57a3847f
Aliesky Perez <aliesky.perez@gmail.com>
2023-05-22 19:06:46 -0400

    implemented api doc with swagger

--- apps/nestjs-backend/src/User/Controller/user.controller.ts
+++ apps/nestjs-backend/src/User/Controller/user.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from '../UseCase/user.service';
 import { CreateUserDTO, ShowCreateUserDTO } from '../Domain/dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { UserObject } from '../Domain';
 
 @Controller('user')
 export class UserController {
@@ -12,6 +14,12 @@ export class UserController {
   }
 
   @Post()
+  @ApiOperation({ summary: 'Crear Usuario' })
+  @ApiResponse({
+    status: 200,
+    description: 'The found record',
+    type: UserObject,
+  })
   async createUser(@Body() params: CreateUserDTO): Promise<ShowCreateUserDTO> {
     return await this.userService.createUser(params);
   }
